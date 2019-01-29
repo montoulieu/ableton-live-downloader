4449de54b846863936ce15f13538e9a25c6551ea
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-29 15:57:00 -0700

    Updating head tags and package info

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Ableton Live Installer Download Utility',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
