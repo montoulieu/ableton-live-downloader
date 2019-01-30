e9ab7d0027b06e259272cfc6fb9fe1561a804fcc
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-29 17:39:10 -0700

    Word changes in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
     <h1 class="font-weight-bold">Ableton Live Installer Download Utility</h1>
-    <p>Downloads the desired <strong>64-bit</strong> installer from all versions of Ableton Live 9 & 10.</p>
+    <p>Downloads the desired <strong>64-bit</strong> installer from any version of Ableton Live 10 or 9.</p>
   </header>
 </template>
