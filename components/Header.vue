089315bd4f2a50d1e3d6cc6e3439896f6374d03d
Pieter Montoulieu <pi.mont@icloud.com>
2019-05-25 17:51:30 -0600

    Name change up

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1 class="font-weight-bold">Ableton Live Installer Download Utility</h1>
+    <h1 class="font-weight-bold">Ableton Live Download Utility</h1>
     <p>Downloads the desired <strong>64-bit</strong> installer from any version of Ableton Live 10 or 9.</p>
   </header>
 </template>
